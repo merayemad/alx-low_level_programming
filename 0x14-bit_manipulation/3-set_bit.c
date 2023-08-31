2bd98dcc9faf22e65f33ef33d0dba7a70d847ead
meray <merayastfan315@gmail.com>
2023-08-31 13:50:50 -0700

    mera

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * clear_bit - clears the bit at the index
+ * set_bit - sets the bit at the index
  * @n: the number to index
- * @index: the bit to clear
+ * @index: the bit to set
  *
  * Return: 1 if successful, -1 on error
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	if (*n & 1L << index)
-		*n ^= 1L << index;
-	return (1);
+	return (!!(*n |= 1L << index));
 }
